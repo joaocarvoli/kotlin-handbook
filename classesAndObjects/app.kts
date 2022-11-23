31ead2b6d7929397e0c6db3b0b5f7a1eff5c71fc
João Victor Carvalho <joaocarvoli@pop-os.localdomain>
2022-11-23 16:53:28 -0300

    Starting to work with classes and objects

---
+++ classesAndObjects/app.kts
@@ -0,0 +1,12 @@
+class Pool {
+    var width : Double? = null
+    var height : Double? = null
+    var lenght : Double? = null
+}
+
+fun main(){
+    var myPool : Pool = Pool()
+    myPool.width = 100.0
+    println(myPool.width)
+}
+
